1fdca7757268dc604f3cabfdfaf806dcb504ccff
uo269948 <uo269948@uniovi.es>
2021-04-27 13:19:19 +0200

    SDI-2021-704-11.2-Servicio SOAP Cliente

---
+++ src/main/java/com/uniovi/Sdi2021704SoapSwApplication.java
@@ -0,0 +1,13 @@
+package com.uniovi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sdi2021704SoapSwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sdi2021704SoapSwApplication.class, args);
+	}
+
+}
